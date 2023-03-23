12a72821a1093da17632a0ae544510eed07d39d7
RafaelPires2 <rafaelbrpires@gmail.com>
2023-03-23 10:43:29 -0300

    feature: emailjs

--- scripts/email.js
+++ scripts/email.js
@@ -13,15 +13,15 @@ export const ContactUs = () => {
       });
   };
 
-  return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-  );
+//   return (
+//     <form ref={form} onSubmit={sendEmail}>
+//       <label>Name</label>
+//       <input type="text" name="user_name" />
+//       <label>Email</label>
+//       <input type="email" name="user_email" />
+//       <label>Message</label>
+//       <textarea name="message" />
+//       <input type="submit" value="Send" />
+//     </form>
+//   );
 };
